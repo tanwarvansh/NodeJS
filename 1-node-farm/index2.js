5a2381d4629e78b605960071dc3819e3ca93a183
Vansh Tanwar <vanshtanwar9@gmail.com>
2024-02-13 18:40:14 +0530

    Adding the routing part

--- 1-node-farm/index2.js
+++ 1-node-farm/index2.js
@@ -1,9 +1,18 @@
 const http = require('http');
 
 const server=http.createServer((req,res)=>{
-    res.end("Hello this is my first server!");
+    const pathName = req.url;
+    if(pathName === '/' || pathName === '/overview'){
+        res.end("Welcome to the Overview Page");
+    }else if (pathName === '/product'){
+        res.end("Welcome to the Product Page");
+    }else{
+        res.writeHead(404,{
+            'Content-type':'text/html'
+        });
+        res.end("<h1 style='color:red'>Page Not Found</h1>");
+    }
 });
-
 server.listen(8080,'127.0.0.1',()=>{
     console.log("server has started running on 8080");
 });
